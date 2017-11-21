464451ebe050ef83275d93fb1b8c7284e95a4439
cjihrig <cjihrig@gmail.com>
2017-11-21 12:31:08 -0500

    add list bridges by account functionality

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const Hapi = require('hapi');
 const Lab = require('lab');
+const Uuid = require('uuid');
 const lab = exports.lab = Lab.script();
 const { describe, it } = lab;
 const { expect } = require('code');
@@ -131,5 +132,56 @@ describe('Minio API', () => {
     expect(data.username).to.equal('jjohnson');
     expect(data.namespace).to.equal('abc123');
     expect(data.directoryMap).to.equal('*:/stor/*');
+  });
+
+  it('lists all bridges for user', async () => {
+    const server = createServer();
+    const accountId = Uuid.v4();
+    const mutation = { query: `
+      mutation {
+        createBridge(bridge: {
+          instanceId: ["1234", "5678"],
+          accountId: "${accountId}",
+          username: "jjohnson",
+          namespace: "abc123",
+          directoryMap: "*:/stor/*",
+          sshKey: "12:c3:de:ad:be:ef",
+          accessKey: "foobar",
+          secretKey: "bazquux"
+        }) {
+          bridgeId
+        }
+      }`
+    };
+    let create = await server.inject({
+      method: 'POST',
+      url: '/graphql',
+      payload: mutation
+    });
+    const bridgeId1 = JSON.parse(create.payload).data.createBridge.bridgeId;
+    create = await server.inject({
+      method: 'POST',
+      url: '/graphql',
+      payload: mutation
+    });
+    const bridgeId2 = JSON.parse(create.payload).data.createBridge.bridgeId;
+    const query = { query: `
+      query {
+        listBridgesByAccount(accountId: "${accountId}") {
+          bridgeId
+        }
+      }`
+    };
+    const res = await server.inject({
+      method: 'POST',
+      url: '/graphql',
+      payload: query
+    });
+    const data = JSON.parse(res.payload).data.listBridgesByAccount;
+
+    expect(data).to.be.an.array();
+    expect(data.length).to.equal(2);
+    expect(data.some((bridge) => { return bridge.bridgeId === bridgeId1; })).to.equal(true);
+    expect(data.some((bridge) => { return bridge.bridgeId === bridgeId2; })).to.equal(true);
   });
 });
